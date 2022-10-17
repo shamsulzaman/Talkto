d5ca04ca0feb1a956a15e99d9589b94c96495509
shamsulzaman <shamsulzaman.s@gmail.com>
2022-10-17 03:35:20 -0230

    worked on the state

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,25 +1,51 @@
+import {Component} from "react"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      monsters:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then((response)=> response.json() //This returns promise
+    ).then((users)=> this.setState(()=>{
+      return {monsters:users}
+    },()=>{console.log(this.state)}))
+  }
+
+
+
+  render(){
+    return (
+      <div className="App">
+
+        <input className="search-box" type="search" placeholder="Search Monsters" onChange={(e)=>{
+          console.log(e)
+          const searchString = e.target.value.toLowerCase()
+          const filterMonsters = this.state.monsters.filter((monster)=>{
+            return monster.name.toLowerCase().includes(searchString)
+          })
+          this.setState(()=>{
+            return {monsters: filterMonsters}
+          })
+        }}/>
+
+        {
+          //this.setState is use to rerender the changes and call back to update the state async
+          this.state.monsters.map((monster)=>{
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
+          })
+        }
+        
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
